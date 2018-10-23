7f5ff8507f8e21c7a0387175f8188107614dac1a
Florian <florian.garcia@veto-soft.com>
2018-10-23 13:35:50 +0200

    creation function getFilmDetailFromApi

--- api/TMBDA.js
+++ api/TMBDA.js
@@ -1,10 +1,15 @@
 const API_TOKEN = '53115dc657f35ace48ea95eb22730c6f';
+const MAIN_URL = 'https://api.themoviedb.org/3/';
 
 export function getFilmsFromApiWithSearchedText(text, page) {
-	const url = `https://api.themoviedb.org/3/search/movie?api_key=${API_TOKEN}&language=fr&query=${text}&page=${page}`;
+	const url = `${MAIN_URL}search/movie?api_key=${API_TOKEN}&language=fr&query=${text}&page=${page}`;
 	return fetch(url)
 		.then(response => response.json())
 		.catch(error => console.log(error));
+}
+
+export function getFilmDetailFromApi(id) {
+	const url = `${MAIN_URL}/movie/${id}?api_key=${API_TOKEN}&language=fr`;
 }
 
 export function getImageFromApi(name) {
